080ab0c213a67ecd7681e8ccb6116cb4348cc10c
Victor <victor.y.kung@gmail.com>
2014-05-03 22:51:30 -0500

    cleaned up code

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ load 'methods.rb'
 
 colors = ['R', 'G', 'B', 'Y', 'O', 'P']
 total_color = colors.length
-total_combo = colors.repeated_permutation(4).to_a 								# all possible code combinations
-guess = [colors.sample, colors.sample, colors.sample, colors.sample]			# first guess of the game (random)
+total_combo = colors.repeated_permutation(4).to_a         # all possible code combinations
+guess = [colors.sample, colors.sample, colors.sample, colors.sample]   # first guess of the game (random)
 tries = 1
 max_tries = 10
 input = ""
@@ -22,67 +22,63 @@ puts "\nWelcome to Mastermind!\n\n"
 puts "INSTRUCTIONS:\nInput a code sequence of 4 random colors from a total of six colors (Red, Green, Blue, Yellow, Orange, Purple) and the computer will try to guess it within 10 turns\n\n"
 puts line_break
 
-
 # User Input Code
 answer = accept_input(4)
 puts line_break
 puts "Your code #{answer} is invisible to the computer. The computer will begin to guess now... \n\n"
 
-
 # Main Loop of the Game
 loop do
+  puts line_break
+  puts "TRY NUMBER: #{tries}\n\n "
 
-	puts line_break
-	puts "TRY NUMBER: #{tries}\n\n "
-
-	# If First Try, Computer Makes a Random Guess
-	if tries == 1
-		puts "Guess is: #{guess}\n\n"
-		score = scoring(answer, guess, colors)
-		puts "Score: #{score}\n\n"
-
-
-		# FILTER THESE BASED ON SCORE
-
-	# If Between Second and Tenth Try
-	else
-
-		# Use Pruning Algorithm to Filter All Possible Combinations by Comparing Them to the Initial Guess and Seeing if the Resulting Score is Equal to the Initial Score
-		total_combo.each do |combo|
-			prune_score = scoring(guess, combo, colors)
-			if prune_score == score
-				prune_combo.push combo
-			end
-		end
-
-		# Pick New Guess from Pruned Combination
-		guess = prune_combo.sample
-		prune_combo.delete(guess)			# delete new guess from the Prune Combination array
-		total_combo = prune_combo 		# reset total_combo to the prune_combo
-		prune_combo = []
-
-		puts "Guess is: #{guess}\n\n"
-		score = scoring(answer, guess, colors)
-		puts "Score: #{score}\n\n"
-
-	end
+  # If First Try, Computer Makes a Random Guess
+  if tries == 1
+    puts "Guess is: #{guess}\n\n"
+    score = scoring(answer, guess, colors)
+    puts "Score: #{score}\n\n"
 
 
-	# Winning
-	if score == [2, 2, 2, 2]
-		puts line_break
-		puts "YOU LOSE!\n\nThe computer guessed the correct sequence #{answer} in just #{tries} guesses.\n\n\n"
-		puts line_break
-		break
-	end
+  # FILTER THESE BASED ON SCORE
 
-	tries += 1
+  # If Between Second and Tenth Try
+  else
+  # Use Pruning Algorithm to Filter All Possible Combinations by Comparing Them to the Initial Guess and Seeing if the Resulting Score is Equal to the Initial Score
+      total_combo.each do |combo|
+      prune_score = scoring(guess, combo, colors)
+        if prune_score == score
+          prune_combo.push combo
+        end
+      end
 
-	if tries > max_tries
-		puts line_break
-		puts "YOU WIN!\n\nYou beat the computer! The computer was unable to guess the code in 10 tries\n\n\n"
-		puts line_break
-		break
-	end
+  # Pick New Guess from Pruned Combination
+    guess = prune_combo.sample
+    prune_combo.delete(guess)   # delete new guess from the Prune Combination array
+    total_combo = prune_combo   # reset total_combo to the prune_combo
+    prune_combo = []
+
+    puts "Guess is: #{guess}\n\n"
+    score = scoring(answer, guess, colors)
+    puts "Score: #{score}\n\n"
+
+  end
+
+
+  # Winning
+  if score == [2, 2, 2, 2]
+    puts line_break
+    puts "YOU LOSE!\n\nThe computer guessed the correct sequence #{answer} in just #{tries} guesses.\n\n\n"
+    puts line_break
+    break
+  end
+
+  tries += 1
+
+  if tries > max_tries
+    puts line_break
+    puts "YOU WIN!\n\nYou beat the computer! The computer was unable to guess the code in 10 tries\n\n\n"
+    puts line_break
+    break
+  end
 
 end
--- methods.rb
+++ methods.rb
@@ -1,27 +1,25 @@
 # PREDEFINED VARIABLES
 
-line_break = "===============================================================================\n\n"
-
 
 # ACCEPTING USER GENERATED CODE
 
-def accept_input(seq_length)
-	print "Enter your 4 sequence color code. Select from R (Red), G (Green), B (Blue), Y (Yellow), O (Orange), P (Purple). \n\nYour input should be in the form: RGBY: "
-	input = gets.chomp
-	input_check = input.gsub(/[RGBYOP]/, "")
-	puts "\n"
+def accept_input(length)
+  print "Enter your 4 sequence color code. Select from R (Red), G (Green), B (Blue), Y (Yellow), O (Orange), P (Purple). \n\nYour input should be in the form: RGBY: "
+  input = gets.chomp
+  input_check = input.gsub(/[RGBYOP]/, "")
+  puts "\n"
 
-	# Account for Incorrect Inputs      ### IMPORTANT ###
-	if input.length != seq_length || input_check.length != 0
-		puts line_break
-		puts "ERROR:\nPlease follow the instructions and enter a 4 sequence color code!\n\n"
-		puts line_break
-		input = ""
-		accept_input(seq_length)
-	else
-		answer = input.split("")      # Storing the input as an array
-	end
-	return answer
+  # Account for Incorrect Inputs      ### IMPORTANT ###
+  if input.length != length || input_check.length != 0
+    puts "===============================================================================\n\n"
+    puts "ERROR:\nPlease follow the instructions and enter a 4 sequence color code!\n\n"
+    puts "===============================================================================\n\n"
+    input = ""
+    answer = accept_input(length)
+  else
+    answer = input.split("")      # Storing the input as an array
+  end
+  return answer
 end
 
 
@@ -29,59 +27,52 @@ end
 
 def scoring(answer, guess, colors)
 
-	# Returning the Number of Correct Color & Positions
-	correct_positions = guess.select.with_index {|x,i| x == answer[i]}
-	# puts "Number of black pegs: #{correct_positions.size}\n"
+  # Returning the Number of Correct Color & Positions
+  correct_positions = guess.select.with_index {|x,i| x == answer[i]}
 
+  # Returning the Number of Correct Colors (not including those with BOTH Correct Color and Position)
+  # By Removing Elements in Each Array that Have the Same Color and Position
+  trimmed_guess = guess.select.with_index {|x,i| x != answer[i]}
+  trimmed_answer = answer.select.with_index {|x,i| x != guess[i]}
 
-	# Returning the Number of Correct Colors (not including those with BOTH Correct Color and Position)
-	# Remove Elements in Each Array that Have the Same Color and Position
-	trimmed_guess = guess.select.with_index {|x,i| x != answer[i]}
-	trimmed_answer = answer.select.with_index {|x,i| x != guess[i]}
+  # Create Hashes to Store the Frequencies of the Modified Arrays
+  input_freq = Hash.new(0)
+  trimmed_guess.each { |color| input_freq[color] += 1}
+  answer_freq = Hash.new(0)
+  trimmed_answer.each { |color| answer_freq[color] += 1}
 
-	# Create Hashes to Store the Frequencies of the Modified Arrays
-	input_freq = Hash.new(0)
-	trimmed_guess.each { |color| input_freq[color] += 1}
-	answer_freq = Hash.new(0)
-	trimmed_answer.each { |color| answer_freq[color] += 1}
+  # Compare the Frequencies in Both Hashes to Calculate Number of Current Colors
+  correct_colors = 0
+  black = 2
+  white = 1
+  neither = 0
 
-	# Compare the Frequencies in Both Hashes to Calculate Number of Current Colors
-	correct_colors = 0
+  colors.each do |c|
+    if answer_freq[c] == input_freq[c] && input_freq[c] == 0
+    elsif answer_freq[c] == input_freq[c] && input_freq[c] != 0
+      correct_colors += [answer_freq[c], input_freq[c]].min
+    elsif (answer_freq[c] != input_freq[c]) && (answer_freq[c] != 0) && (input_freq[c] != 0)
+      correct_colors += [answer_freq[c], input_freq[c]].min
+    end
+  end
 
-	colors.each do |c|
-		if answer_freq[c] == input_freq[c] && input_freq[c] == 0
-		elsif answer_freq[c] == input_freq[c] && input_freq[c] != 0
-			correct_colors += [answer_freq[c], input_freq[c]].min
-		elsif (answer_freq[c] != input_freq[c]) && (answer_freq[c] != 0) && (input_freq[c] != 0)
-			correct_colors += [answer_freq[c], input_freq[c]].min
-		end
-	end
-	# puts "Number of white pegs: #{correct_colors}\n\n"
+  # Creating the Score Array
+  score = Array.new
 
-	# Creating the Score Array
-	score = Array.new
+  correct_positions.size.times do
+  score.push black
+  end
 
-	correct_positions.size.times do
-	  black = 2
-	  score.push black
-	end
+  correct_colors.times do
+  score.push white
+  end
 
-	correct_colors.times do
-		white = 1
-		score.push white
-	end
+  num_neither = 4 - correct_positions.size - correct_colors
 
-	num_neither = 4 - correct_positions.size - correct_colors
+  num_neither.times do
+  score.push neither
+  end
 
-	if num_neither > 0
-		num_neither.times do
-			neither = 0
-			score.push neither
-		end
-	end
-
-	return score
+  return score
 
 end
-
-
